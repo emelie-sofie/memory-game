2fde6bc8d53e1ebe50fa82b71229707569302f2c
Emelie Karlsson <emelie-sofie@users.noreply.github.com>
2017-09-26 09:22:09 +0200

    First commit

---
+++ src/game.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Card from './card'
+import './game.css'
+
+const Game = () => (
+  <div className="game">
+    <h1>Memory Game</h1>
+    <Card src="/images/kitten-1.jpg"/>
+    <Card src="/images/kitten-2.jpg"/>
+    <Card src="/images/kitten-3.jpg"/>
+    <Card src="/images/kitten-4.jpg"/>
+    <Card src="/images/kitten-5.jpg"/>
+    <Card src="/images/kitten-6.jpg"/>
+    <Card src="/images/kitten-1.jpg"/>
+    <Card src="/images/kitten-2.jpg"/>
+    <Card src="/images/kitten-3.jpg"/>
+    <Card src="/images/kitten-4.jpg"/>
+    <Card src="/images/kitten-5.jpg"/>
+    <Card src="/images/kitten-6.jpg"/>
+  </div>
+)
+
+export default Game
